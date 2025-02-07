c39d7a7020e66135f5b894ddeaf3c71b738b062f
ALISONdantas <147450340+ALISONdantas@users.noreply.github.com>
2025-02-07 11:14:46 -0300

    Update Produto.java

--- estoque/Produto.java
+++ estoque/Produto.java
@@ -1,1 +1,30 @@
+package estoque;
 
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private double preco;
+
+    public Produto(int id, String nome, String descricao, int quantidade, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    // Getters e Setters
+    public void adicionarQuantidade(int quantidade) {
+        this.quantidade += quantidade;
+    }
+
+    public void removerQuantidade(int quantidade) {
+        this.quantidade -= quantidade;
+    }
+
+    public boolean verificarEstoqueMinimo(int minimo) {
+        return this.quantidade < minimo;
+    }
+}
