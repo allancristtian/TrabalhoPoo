95fcb99348f74fa51bebf35d50f702bcb181e7a6
ALISONdantas <147450340+ALISONdantas@users.noreply.github.com>
2025-02-07 11:16:07 -0300

    Update Usuario.java

--- usuario/Usuario.java
+++ usuario/Usuario.java
@@ -1,1 +1,20 @@
 
+package usuarios;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String login;
+    private String senha;
+    private String permissao;
+
+    public Usuario(int id, String nome, String login, String senha, String permissao) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.permissao = permissao;
+    }
+
+    // Getters e Setters
+}
