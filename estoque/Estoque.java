9d301284a35df2c45d970166bb4be303448daf12
ALISONdantas <147450340+ALISONdantas@users.noreply.github.com>
2025-02-07 11:15:10 -0300

    Update Estoque.java

--- estoque/Estoque.java
+++ estoque/Estoque.java
@@ -1,1 +1,26 @@
+package estoque;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estoque {
+    private List<Produto> produtos;
+
+    public Estoque() {
+        this.produtos = new ArrayList<>();
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void removerProduto(int id) {
+        produtos.removeIf(p -> p.getId() == id);
+    }
+
+    public void gerarRelatorio() {
+        for (Produto p : produtos) {
+            System.out.println(p.getNome() + " - Quantidade: " + p.getQuantidade());
+        }
+    }
+}
