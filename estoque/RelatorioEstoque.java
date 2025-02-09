7b063d2b482b001a22a787ce15f06af8859f3328
davikelton <davikelton03@gmail.com>
2025-02-08 22:46:11 -0300

    Update RelatorioEstoque.java

--- estoque/RelatorioEstoque.java
+++ estoque/RelatorioEstoque.java
@@ -1,16 +1,16 @@
 public class Movimentacao {
-    private String tipo; 
+    private String tipo;
     private int quantidade;
+    private int quantidadeAnterior; 
 
-    
-    public Movimentacao(String tipo, int quantidade) {
+    public Movimentacao(String tipo, int quantidade, int quantidadeAnterior) {
         this.tipo = tipo;
         this.quantidade = quantidade;
+        this.quantidadeAnterior = quantidadeAnterior;
     }
 
     @Override
     public String toString() {
-        return String.format("Quantidade: %d, Tipo: %s", quantidade, tipo);
+        return String.format("Tipo: %s, Quantidade: %d, Quantidade Anterior: %d", tipo, quantidade, quantidadeAnterior);
     }
 }
-
